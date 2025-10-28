cec6b6a0654881fbf98eee6f8f6d5b7e9d44add4
y9mfn 💱 <yamin.ahmed.2@city.ac.uk>
2025-10-28 15:38:02 +0000

    Trace vehicle_types bad boolean comparison: add DB::listen in AppServiceProvider

--- framework/app/Providers/AppServiceProvider.php
+++ framework/app/Providers/AppServiceProvider.php
@@ -11,6 +11,8 @@ Design and developed by Hyvikk Solutions <https://hyvikk.com/>
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 use Laravel\Passport\Console\ClientCommand;
 use Laravel\Passport\Console\InstallCommand;
 use Laravel\Passport\Console\KeysCommand;
@@ -27,6 +29,18 @@ class AppServiceProvider extends ServiceProvider {
                 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
                     \URL::forceScheme('https');
                 }
+
+                // Temporary: trace any query comparing vehicle_types.is_enabled to integer 1
+                DB::listen(function ($query) {
+                    if (Str::contains($query->sql, 'from "vehicle_types"') &&
+                        Str::contains($query->sql, '"is_enabled" = 1')) {
+                        \Log::error('TRACE vehicle_types is_enabled = 1', [
+                            'sql' => $query->sql,
+                            'bindings' => $query->bindings,
+                            'trace' => (new \Exception())->getTraceAsString(),
+                        ]);
+                    }
+                });
         }
 
         /**

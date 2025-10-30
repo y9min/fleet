e97e365d4e78aa7342ac3020b9e36f6d561b8289
y9mfn 💱 <yamin.ahmed.2@city.ac.uk>
2025-10-30 14:29:23 +0000

    chore(feature-flag): disable Vehicle Groups via config; guard routes, hide menu, remove group fields from vehicle forms and details; add safe redirects

--- framework/resources/views/layouts/app.blade.php
+++ framework/resources/views/layouts/app.blade.php
@@ -1956,15 +1956,17 @@ input:checked + .slider:before {
                     </a>
                   </li>
                   @endcan --}}
-                  @can('VehicleGroup list')
-                  <li class="nav-item">
-                    <a href="{{ route('vehicle_group.index')}}"
-                      class="nav-link @if(Request::is('admin/vehicle_group*')) active @endif">
-                      <i class="fa fa-inbox nav-icon"></i>
-                      <p>@lang('fleet.manageGroup')</p>
-                    </a>
-                  </li>
-                  @endcan
+                  @if(config('features.vehicle_groups'))
+                    @can('VehicleGroup list')
+                    <li class="nav-item">
+                      <a href="{{ route('vehicle_group.index')}}"
+                        class="nav-link @if(Request::is('admin/vehicle_group*')) active @endif">
+                        <i class="fa fa-inbox nav-icon"></i>
+                        <p>@lang('fleet.manageGroup')</p>
+                      </a>
+                    </li>
+                    @endcan
+                  @endif
                   @can('VehicleInspection list')
                   <li class="nav-item">
                     <a href="{{ url('admin/vehicle-reviews')}}"

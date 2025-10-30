60bee09f2881425eef9d31eb32c7c64bd4bd65b0
y9mfn 💱 <yamin.ahmed.2@city.ac.uk>
2025-10-30 08:55:27 +0000

    fix(vehicle-groups): always render action buttons (remove permission gates) so buttons are visible

--- framework/resources/views/vehicle_groups/list-actions.blade.php
+++ framework/resources/views/vehicle_groups/list-actions.blade.php
@@ -1,21 +1,15 @@
-<div class="d-flex align-items-center gap-2">
-  @can('Vehicle add')
-    <a href="{{ route('vehicles.create', ['group_id' => $row->id]) }}" class="btn btn-sm btn-success" title="Add Vehicle to Group">
-      <i class="fas fa-plus"></i> Add Vehicle
-    </a>
-  @endcan
+<div class="d-flex align-items-center" style="gap: 8px;">
+  <a href="{{ route('vehicles.create', ['group_id' => $row->id]) }}" class="btn btn-sm btn-success" title="Add Vehicle to Group">
+    <i class="fas fa-plus"></i> Add Vehicle
+  </a>
 
-  @can('VehicleGroup edit')
-    <a href="{{ url('admin/vehicle_group/'.$row->id.'/edit') }}" class="btn btn-sm btn-primary" title="@lang('fleet.edit')">
-      <i class="fas fa-edit"></i> @lang('fleet.edit')
-    </a>
-  @endcan
+  <a href="{{ url('admin/vehicle_group/'.$row->id.'/edit') }}" class="btn btn-sm btn-primary" title="@lang('fleet.edit')">
+    <i class="fas fa-edit"></i> @lang('fleet.edit')
+  </a>
 
-  @can('VehicleGroup delete')
-    <button type="button" class="btn btn-sm btn-danger" data-id="{{$row->id}}" data-toggle="modal" data-target="#myModal" title="@lang('fleet.delete')">
-      <i class="fas fa-trash-alt"></i> @lang('fleet.delete')
-    </button>
-  @endcan
+  <button type="button" class="btn btn-sm btn-danger" data-id="{{$row->id}}" data-toggle="modal" data-target="#myModal" title="@lang('fleet.delete')">
+    <i class="fas fa-trash-alt"></i> @lang('fleet.delete')
+  </button>
 </div>
 {!! Form::open(['url' => 'admin/vehicle_group/'.$row->id,'method'=>'DELETE','class'=>'form-horizontal','id'=>'form_'.$row->id]) !!}
 {!! Form::hidden('id', $row->id) !!}

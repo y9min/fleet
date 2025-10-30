4eb6bba84e8f0f41f5cbb209d0eedfb91c47e1ea
y9mfn 💱 <yamin.ahmed.2@city.ac.uk>
2025-10-30 13:51:00 +0000

    chore(vehicle-groups): remove icon from Create Vehicle Group modal title

--- framework/resources/views/vehicle_groups/index.blade.php
+++ framework/resources/views/vehicle_groups/index.blade.php
@@ -592,7 +592,7 @@ body {
   <div class="modal-dialog modal-xl">
     <div class="modal-content">
       <div class="modal-header">
-        <h4 class="modal-title"><i class="fas fa-object-group"></i> Create Vehicle Group</h4>
+        <h4 class="modal-title">Create Vehicle Group</h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
       </div>
       <div class="modal-body">
